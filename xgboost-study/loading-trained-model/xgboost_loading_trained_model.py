29dc4badc0741adab02a3edfcf5fe96f2009db81
gabastil <glenn.abastillas@gmail.com>
2020-03-23 17:23:24 -0400

    Added study script for xgboost loading a trained model study

---
+++ xgboost-study/loading-trained-model/xgboost_loading_trained_model.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+#-*- encoding: utf-8 -*-
+# filename : xgboost_loading_trained_model.py
+# author : glenn abastillas
+# created : march 23, 2020
+# description : file to inspect xgboost package loading trained model from
+#   another package
+
+
+import xgboost as xgb
+import sklearn
+
+
+# implement code
